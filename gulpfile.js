0683f3ec1b9d6192d18ba97ccdec2c38df741705
Swatantra Sinha <swasinha1@publicisgroupe.net>
2018-07-11 16:19:29 +0530

    Push After Review Comments - 1

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,19 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
-//compile by scss to css and place in my src folder
+//here it will compile all scss files from scss folder to corresponding css
 gulp.task('sass', () => {
-
     return gulp
-
         .src(['scss/*.scss'])
-
-        .pipe(sass())
-
+        .pipe(sass())     // Its like filtering and passing to sass
         .pipe(gulp.dest('css'))
-
-        .pipe(browserSync.stream());
-
+        .pipe(browserSync.stream());   // whatever css created need to be sync to browser
 })
 
-gulp.task('js', () => {
-
+gulp.task('js', () => {   //take popper.jQuery and bootstrap from node module
     return gulp
-
         .src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/jquery.min.js', 'node_modules/popper.js'])
-
         .pipe(gulp.dest('js'))
-
         .pipe(browserSync.stream());
 
 })
@@ -32,18 +22,12 @@ gulp.task('js', () => {
 // Static Server + watching scss/html files
 
 gulp.task('serve', ['sass'], function () {
-
     browserSync.init({
-
         server: '.'
-
     });
 
-    gulp.watch("scss/*.scss", ['sass']);
-
-    gulp.watch("*.html").on('change', browserSync.reload);
-
+    gulp.watch("scss/*.scss", ['sass']);   //check for update in scss
+    gulp.watch("*.html").on('change', browserSync.reload); //check for update in html
 });
-
 
 gulp.task('default', ['serve', 'js']);
